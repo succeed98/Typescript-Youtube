a4765a3fc9ac71e01abb7233cdf6847cf729738e
Fourteen98 <emuhiydeen6@gmail.com>
2022-12-07 12:33:19 +0000

    finish function

--- 02basics/myfunction.ts
+++ 02basics/myfunction.ts
@@ -1,4 +1,4 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2
 }
 
@@ -16,4 +16,30 @@ let myVal = addTwo(5)
 getUpper("Elias")
 signUpUser("Elias", "elias@gmail.com", "28384", true)
 logInUser("elias", "elias@gmail.com")
+
+function getValue(myVal: number) {
+  if (myVal > 5) {
+    return true
+  }
+  return "200 OK"
+}
+
+const gettHello = (s: string):string => {
+  return ""
+}
+
+const heroes = ["thor", "spiderman", "ironman"]
+// const heroes = [1, 2, 4]
+
+heroes.map((hero): string => { return `hero is ${hero}` })
+
+
+function consoleError(errmsg): void {
+  console.log(errmsg)
+}
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg)
+}
+
 export { }
