e814858a00cef5aaa8e6494f1554e53e715753e8
Fourteen98 <emuhiydeen6@gmail.com>
2022-12-18 09:51:55 +0000

    add generics

---
+++ 03purets/dist/myGeneric.js
@@ -0,0 +1,17 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+// identityThree(true)
+function identityFour(val) {
+    return val;
+}
+identityFour({ brand: "shortcut", type: 4 });
